9312f6a3aff64466ef3375cc7ddb2353c139b9dc
Leo Ruiz <ruizruizleo@hotmail.com>
2022-10-10 20:36:01 -0300

    Feat: Usuario Model

---
+++ src/model/Usuario.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema({
+    nome: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    senha: { type: String, required: true },
+    imagem: { type: String, required: true },
+    enderecos: [
+        {
+            rua: { type: String, required: true},
+            numero: { type: Number, required: true},
+            complemento: { type: String, required: false},
+            CEP: { type: String, required: true},
+            createdAt: { type: Date, required: true},
+        }
+    ],
+    createdAt: { type: Date, required: true},
+    produtos_fav: [
+        {
+            _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "produtos"},
+            createdAt: { type: Date, required: true},
+        }
+    ],
+    admin: { type: Boolean, required: true, default: false },
+});
+
+const Usuario = mongoose.model("usuarios", UsuarioSchema);
+
+module.exports = Usuario;
