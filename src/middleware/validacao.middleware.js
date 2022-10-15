620fb90099669240bd4a1ff025ffc03ea4bf91fa
Leo Ruiz <ruizruizleo@hotmail.com>
2022-10-15 12:38:19 -0300

    Feat: valida login implementado

--- src/middleware/validacao.middleware.js
+++ src/middleware/validacao.middleware.js
@@ -122,11 +122,35 @@ const validaId = (req, res, next) => {
     }
 }
 
+const validaLogin = (req, res, next) => {
+    let erros = [];
+    
+    if(!req.body.email){
+        erros.push("email");
+    }
+
+    if(!req.body.senha){
+        erros.push("senha");
+    }
+
+    //testando quantos erros temos e tomando decisoes em relacao a isso
+    if(erros.length == 0){
+        return next();
+    }else{
+        if(erros. length > 1){
+            return res.status(400).send({ message: `Os campos ${erros} precisam ser preenchidos!`});
+        }else{
+            return res.status(400).send({ message: `O campo ${erros} precisa ser preenchido!`});
+        }
+    }
+}
+
 module.exports = {
     validaUsuario,
     validaProduto,
     validaCategoria,
     validaPedido,
     validaCarrinho,
-    validaId
+    validaId,
+    validaLogin
 }
