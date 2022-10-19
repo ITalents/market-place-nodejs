8b41ef7693cff41f334f4b9edd7cf67a9c8b08ee
Leo Ruiz <ruizruizleo@hotmail.com>
2022-10-19 15:40:09 -0300

    Fix: CRUD Usuario

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -31,12 +31,6 @@ const findAllUsersController = async (req, res) => {
 
 const createUserController = async (req, res) => {
     try{
-        const body = req.body;
-        
-        if(!body.nome){
-            return res.status(400).send({ message: `O campo 'nome' precisa ser preenchido!`});
-        }
-
         return res.status(201).send(await userService.createUserService(body));
     }catch (err){
         console.log(`erro: ${err.message}`);
