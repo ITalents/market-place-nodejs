4633c8bd090a6280677e7ba975fe629d6bca18b9
Leo Ruiz <ruizruizleo@hotmail.com>
2022-10-21 00:55:25 -0300

    Feat: Impletacao do CORS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+
 const connectToDatabase = require("./src/database/database"); //arquivo de conexao com o banco
 
 const usuario = require("./src/router/usuario.router"); //arquivo de rota do usuario
@@ -15,6 +17,15 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors(
+    {
+        origin: [
+            "localhost:3001",
+            "localhost:3002"
+        ],
+        methods: ["GET","POST","PUT", "PATCH","DELETE"]
+    }
+));
 
 connectToDatabase(); //conectando com o banco
 
